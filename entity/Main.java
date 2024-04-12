6116690c11b9016b5f6d81afc7fe015c75e4180b
Juan Pablo <j.samso@alumno.um.edu.ar>
2024-04-12 19:46:45 -0300

    1.1

---
+++ entity/Main.java
@@ -0,0 +1,9 @@
+package entity;
+
+public class Main {
+    public static void main(String[] args) {
+        Ingrediente huevo = new Ingrediente("Huevo", 10);
+
+        System.out.println(huevo);
+    }
+}
